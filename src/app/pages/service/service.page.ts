6e9f8ec212bd4d51455347648dc715e5d08ed225
Steveng1997 <stivengarcia1855@gmail.com>
2024-03-25 10:06:04 -0500

    change 8

--- src/app/pages/service/service.page.ts
+++ src/app/pages/service/service.page.ts
@@ -7,17 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ServicePage implements OnInit {
-
-  showInfo: boolean = false
+  details: boolean = true
 
   constructor() { }
 
   ngOnInit() {
-    this.showInfo = false
+    // this.details = false
   }
 
-  showDetails() {
-    if (this.showInfo == false) this.showInfo = true
-    else this.showInfo = false
+  aqui() {
+    if (this.details == false) {
+      this.details = true
+      document.getElementById('segundo').style.top = 'calc(70% - 163px)'
+      document.getElementById('segundo2').style.top = 'calc(70% - 163px)'
+      document.getElementById('segundo3').style.top = 'calc(70% - 163px)'
+      document.getElementById('segundo4').style.top = 'calc(70% - 163px)'
+      document.getElementById('segundo5').style.top = 'calc(70% - 163px)'
+    } else {
+      document.getElementById('segundo').style.top = '41px'
+      document.getElementById('segundo2').style.top = '102px'
+      document.getElementById('segundo3').style.top = '102px'
+      document.getElementById('segundo4').style.top = '102px'
+      document.getElementById('segundo5').style.top = '102px'
+      this.details = false
+    }
   }
 }
