3d76efae6eb43e93b726fb2d955bce7392d4de61
Steveng1997 <stivengarcia1855@gmail.com>
2024-05-09 00:12:13 -0500

    change 90

--- src/app/pages/liquidation/therapist/new-liquid-therap/new-liquid-therap.module.ts
+++ src/app/pages/liquidation/therapist/new-liquid-therap/new-liquid-therap.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { NewLiquidTherapPageRoutingModule } from './new-liquid-therap-routing.module';
 import { NewLiquidTherapPage } from './new-liquid-therap.page';
@@ -9,6 +9,7 @@ import { NewLiquidTherapPage } from './new-liquid-therap.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     NewLiquidTherapPageRoutingModule
   ],
--- src/app/pages/liquidation/therapist/new-liquid-therap/new-liquid-therap.page.ts
+++ src/app/pages/liquidation/therapist/new-liquid-therap/new-liquid-therap.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 // Service
 import { IonLoaderService } from 'src/app/core/services/loading/ion-loader.service';
+import { TherapistService } from 'src/app/core/services/therapist/therapist.service';
 
 @Component({
   selector: 'app-new-liquid-therap',
@@ -13,19 +14,35 @@ import { IonLoaderService } from 'src/app/core/services/loading/ion-loader.servi
 export class NewLiquidTherapPage implements OnInit {
 
   id: number
+  terapeuta: any
+  dates: boolean = false
 
   constructor(
     public router: Router,
     private activatedRoute: ActivatedRoute,
     private ionLoaderService: IonLoaderService,
+    private serviceTherapist: TherapistService,
   ) { }
 
   ngOnInit() {
     const params = this.activatedRoute.snapshot['_routerState']['_root']['children'][0]['value']['params'];
     this.id = Number(params['id'])
+
+    this.getTherapist()
+  }
+
+  getTherapist() {
+    this.serviceTherapist.getAllTerapeuta().subscribe((datosTerapeuta: any) => {
+      this.terapeuta = datosTerapeuta
+    })
   }
 
   back() {
     this.router.navigate([`tabs/${this.id}/liquidation-therapist`])
+  }
+
+  async inputDateAndTime() {
+    this.dates = true
+    document.getElementById('overview').style.height = '4065px'
   }
 }
